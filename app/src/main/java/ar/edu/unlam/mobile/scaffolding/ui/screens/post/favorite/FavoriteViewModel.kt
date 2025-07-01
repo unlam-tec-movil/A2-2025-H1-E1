4d7107797c7519b00578b5abf4b6407bca53c035
Franco Viscarret <francoviscarret@gmail.com>
2025-06-30 22:56:20 -0300

    SDK y elimino una coma en parametro

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/post/favorite/FavoriteViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/post/favorite/FavoriteViewModel.kt
@@ -11,11 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FavoriteViewModel
-    @Inject
-    constructor(
-        private val repository: FavoriteRepository
-    ) : ViewModel() {
+class FavoriteViewModel @Inject constructor(private val repository: FavoriteRepository) : ViewModel() {
         private val _favorites = MutableStateFlow<List<Tuit>>(emptyList())
         val favorites: StateFlow<List<Tuit>> = _favorites.asStateFlow()
 
