5194d57aa0a6863eb4771c03569daba5d0a9d459
ImaaValenzuela <imanolvalenzuela03@gmail.com>
2025-07-02 15:26:48 -0300

    Fix User

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/user/UserScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/user/UserScreen.kt
@@ -78,7 +78,7 @@ fun UserScreen(
                     .fillMaxWidth(),
         )
 
-        val isCurrentUser = (userState as? Success)?.user?.id == currentUserId
+        val isCurrentUser = (userState as? Success)?.user?.userId == currentUserId
 
         Box(
             modifier =
